70cbd676c7f3710f2d975fd83078505d3fe16457
Tyler Aldrich <tyler@tyleraldrich.cat>
2023-04-14 15:41:10 -0400

    Day 1, solution 2

--- src/solutions/year_2022/day1/solution.rs
+++ src/solutions/year_2022/day1/solution.rs
@@ -1,16 +1,5 @@
 pub fn solution_one(input: Vec<String>) {
-    let mut elves: Vec<usize> = Vec::new();
-    let mut total_calories = 0;
-    for value in input {
-        if value == "" {
-            elves.push(total_calories);
-            total_calories = 0;
-        } else {
-            let cals = value.parse::<usize>().expect("Invalid number found");
-            total_calories += cals;
-        }
-    }
-    elves.push(total_calories);
+    let elves = elf_calories(input);
 
     let mut max = 0;
     for elf in elves {
@@ -23,5 +12,28 @@ pub fn solution_one(input: Vec<String>) {
 }
 
 pub fn solution_two(input: Vec<String>) {
-    println!("{:?}", input);
+    let mut elves = elf_calories(input);
+    elves.sort_by(|a, b| b.cmp(a));
+
+    if elves.len() < 3 {
+        println!("Unable to compute answer - amount of elves is less than 3.");
+    } else {
+        println!("Solution: {}", elves[0] + elves[1] + elves[2]);
+    }
+}
+
+fn elf_calories(input: Vec<String>) -> Vec<usize> {
+    let mut elves: Vec<usize> = Vec::new();
+    let mut total_calories = 0;
+    for value in input {
+        if value == "" {
+            elves.push(total_calories);
+            total_calories = 0;
+        } else {
+            let cals = value.parse::<usize>().expect("Invalid number found");
+            total_calories += cals;
+        }
+    }
+    elves.push(total_calories);
+    elves
 }
